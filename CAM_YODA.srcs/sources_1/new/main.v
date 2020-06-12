86b78a67383dba39236cf6b9f15e04f0683cc395
JamieWonderchild <jamie.aronson00@gmail.com>
2020-06-12 14:54:43 +0200

    updated main

--- CAM_YODA.srcs/sources_1/new/main.v
+++ CAM_YODA.srcs/sources_1/new/main.v
@@ -22,6 +22,7 @@
 module main(
     input	clock, 
 	input  btn_next, //next record button 
+	input btn_rst,
 	input [15:0]SW,
 	
 	output reg [7:0]SEG,
@@ -29,7 +30,13 @@ module main(
     );
     
     wire inc_button;
+    wire rst_btn;
+    
 	Debounce debounce_inc(.clk(clock), .button(btn_next), .out(inc_button)); 
+	Debounce debounce_rst(.clk(clock), .button(btn_rst), .out(rst)); 
+	
+	wire mem_addr;
+	search search_record(.clk(clock), .user_input(), .reset(rst_btn), next, start, we, oe, cs,)
     
     always @(posedge clock)begin
     //split input twitches into [7:0] and [15:8}
--- hash.c
+++ hash.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 
+int main() {
+  printf("Enter in data to be hashed: " );
+  char data;
+  scanf("%c", data);
+  printf("the has is %u\n", hash(data));
+  return 0;
+}
+
 int hash(char s){
   int h = 0;
   for(int i=0; i<s.length();i++){
