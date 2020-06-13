bbcd205ff87ed677a945f9aaa9a33733ec3da8ac
Jonny <jfriedman97@gmail.com>
2020-06-13 15:39:08 +0200

    updated search with we/re

--- CAM_YODA.srcs/sources_1/new/search.v
+++ CAM_YODA.srcs/sources_1/new/search.v
@@ -1,19 +1,18 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-//TO DO:
-//Take out for loop (tick)
-// increment in always loop (tick)
-//allow for searching of individual characters
+
 //////////////////////////////////////////////////////////////////////////////////
 module search(
-input clk, user_input, reset, next, start, cs, we, w_addr, re, w_data, //user input  = sw, next and start are buttons
+input clk, user_input, start_add, we, re, w_data, next, //user input  = sw, next and start are buttons
     output data_out // output returns an address in binary up to 560 or 1111111111 to signify no match found
     );
     reg [15:0] mem_add = 0;
+    reg [15:0] stor_add = 0;
+    reg [15:0] lastDataStored=0;
+    reg cs = 0;
     reg found;
     reg data;
     reg out = 0;
-    reg started;
     reg [15:0] char1;
     reg [15:0] char2;
     reg [15:0] input1;
@@ -31,25 +30,39 @@ input clk, user_input, reset, next, start, cs, we, w_addr, re, w_data, //user in
     );
     
 always@(posedge clk)begin
-    if(reset)begin//reset conditions
-    //is this necesary if not main module?
+    cs = 0;
+    if(found&next)begin
+    found <=0;
+    mem_add = start_add;
     end
-    if(start || started)begin//wait for button push to start
-        started = 1;
-        char1 = data & 16'b1111111100000000;
-        char1 = char1 >> 8;
-        char2 = data & 16'b0000000011111111;
-        input1 = user_input & 16'b1111111100000000;
-        input1 = input1 >> 8;
-        input2 = user_input & 16'b0000000011111111;
+    
+    if(re&(~found))begin//read enable mode
+        cs = 1;
+        char1 <= data & 16'b1111111100000000;
+        char1 <= char1 >> 8;
+        char2 <= data & 16'b0000000011111111;
+        input1 <= user_input & 16'b1111111100000000;
+        input1 <= input1 >> 8;
+        input2 <= user_input & 16'b0000000011111111;
         if(input1)begin
-            if(input1 == char1)begin
-                out <= mem_add;
-                found <= 1;
-            end else if(input1 == char2)begin
-                out <= mem_add;
-                found <= 1;
-            end else if(input2 == char1)begin
+            if(input2==0)begin//input 1 but no input 2
+                if(input1 == char1)begin
+                   out <= mem_add;
+                   found <= 1;
+                end else if(input1 == char2)begin
+                   out <= mem_add;
+                   found <= 1;
+            end else begin//input 1 and 2
+                if(input1 == char1)begin
+                    if(input2 == char2)begin
+                        out <= mem_add;
+                        found <= 1;
+                    end
+                end
+            end
+             
+        end else if (input2)begin//input 2 but no input 1
+            if(input2 == char1)begin
                 out <= mem_add;
                 found <= 1;
             end else if(input2 == char2)begin
@@ -57,17 +70,16 @@ always@(posedge clk)begin
                 found <= 1;
             end
         end
-      
-        while(found)begin//stays in loop until button is pressed
-            //print to seven segment and wait for button push(next)
-            if(next)begin
-                found <= 0;
-            end
+    end else if (we&(~re))begin//write enable mode
+        if(w_data!=lastDataStored)begin
+            lastDataStored <= w_data;
+            cs <= 1;
         end
+    end
+      
         if(mem_add == 560)begin
             out <= 'b1111111111;
-            started = 0;
-            mem_add = 0;
+            mem_add <= 0;
         end
     end 
 mem_add = mem_add+1;
